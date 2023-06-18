299006c089a6755b505a57fff56ac5d876c4d8fe
a-min salehi <104736857+a-min-salehi@users.noreply.github.com>
2023-06-18 14:02:35 +0330

    Create 12_1.py

---
+++ 12/12_1.py
@@ -0,0 +1,15 @@
+def print_lyrics():
+    print('I\'m a lumberjack , and i\'m okay.\nI sleep all night and work all day')
+
+
+def repeat_lyrics() :
+    print_lyrics()
+    print()
+    print_lyrics()
+
+def greeting(name) :
+    print(f' Hello {name}\n how are you ?')
+    
+def do_twice(f):
+    f()
+    f()
